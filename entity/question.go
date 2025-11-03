db1077bb4a2ad0c31aa4f788e88cef119f1f35ca
barbod imani <barbodimani8181@gmail.com>
2025-11-03 20:19:55 +0330

    defined core entities

---
+++ entity/question.go
@@ -0,0 +1,49 @@
+package entity
+
+type Question struct {
+	ID              int
+	Text            string
+	PossibleAnswers []PossibleAnswer
+	CorrectAnswerID uint
+	Difficulty      QuestionDifficulty
+	CategoryID      uint
+}
+
+type PossibleAnswer struct {
+	ID     uint
+	Text   string
+	Choice PossibleAnswerChoice
+}
+
+type PossibleAnswerChoice uint8
+
+func (p PossibleAnswerChoice) IsValid() bool {
+	if p < PossibleAnswerA || p > PossibleAnswerD {
+		return false
+	}
+
+	return true
+}
+
+const (
+	PossibleAnswerA PossibleAnswerChoice = iota + 1
+	PossibleAnswerB
+	PossibleAnswerC
+	PossibleAnswerD
+)
+
+type QuestionDifficulty uint8
+
+const (
+	QuestionDifficultyEasy QuestionDifficulty = iota + 1
+	QuestionDifficultyMedium
+	QuestionDifficultyHard
+)
+
+func (q QuestionDifficulty) IsValid() bool {
+	if q < QuestionDifficultyEasy || q > QuestionDifficultyHard {
+		return false
+	}
+
+	return true
+}
---
+++ requirements.md
@@ -0,0 +1,41 @@
+# Game Application
+
+
+# use-case
+
+## User use-cases
+
+### Register
+User can register via phone number
+
+### Login
+User can login via phone number and password
+
+## Game use-cases
+### Each game has a given number of questions
+### The difficulty levels are "easy", "medium", "hard"
+### Game winner is determined by the correct answers
+### Each game belongs to a category: sport, history, etc
+
+# entity
+ 
+## User
+- ID 
+- Phone Number
+- Avatar
+- Name
+
+## Game
+- ID
+- Category
+- Question List
+- Players
+- Winner
+
+## Question
+- ID
+- Question
+- Answer List
+- Correct Answer
+- Difficulty
+- Category
